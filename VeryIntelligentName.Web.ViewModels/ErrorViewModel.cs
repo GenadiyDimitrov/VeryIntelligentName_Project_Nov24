f13acb0c4e7bf2fc236a76bc584e8f43749d8b54
Genadiy Dimitrov <genadiidimitrov@gmail.com>
2024-11-20 14:09:34 +0200

    Add project files.

---
+++ VeryIntelligentName.Web.ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VeryIntelligentName.Web.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
