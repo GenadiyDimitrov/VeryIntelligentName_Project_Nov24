a45c13a2a55403b35b5683ab6d5993e2218f1d80
Genadiy Dimitrov <genadiidimitrov@gmail.com>
2024-11-20 15:59:36 +0200

    Moved appsetings to secrets

--- VeryIntelligentName.Web/Program.cs
+++ VeryIntelligentName.Web/Program.cs
@@ -5,7 +5,7 @@ using VeryIntelligentName.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("DBConnection") ?? throw new InvalidOperationException("Connection string 'DBConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
